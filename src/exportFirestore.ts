ea60f99b87c16e819ea46ce9115b71298f58b186
t-kojima <t-kojima@sonicgarden.jp>
2024-03-12 17:00:02 +0900

    feat: firebase-functionsの依存を排除して、関数として公開する

--- src/exportFirestore.ts
+++ src/exportFirestore.ts
@@ -1,36 +1,19 @@
-import * as functions from 'firebase-functions';
 import * as firestore from '@google-cloud/firestore';
 const client = new firestore.v1.FirestoreAdminClient();
 
-export const exportFirestore = ({
-  region = 'asia-northeast1',
-  schedule = '0 0 * * *',
-  timeZone = 'Asia/Tokyo',
+export const exportFirestore = async ({
   projectId = process.env.GCLOUD_PROJECT,
   bucketName,
 }: {
-  region?: string;
-  schedule?: string;
-  timeZone?: string;
   projectId?: string;
   bucketName?: string;
-}): functions.CloudFunction<unknown> => {
+} = {}): Promise<void> => {
   const bucket = bucketName ? `gs://${bucketName}` : `gs://${projectId}-firestore-backups`;
-  return functions
-    .region(region)
-    .pubsub.schedule(schedule)
-    .timeZone(timeZone)
-    .onRun(async () => {
-      try {
-        const databaseName = client.databasePath(projectId, '(default)');
+  const databaseName = client.databasePath(projectId, '(default)');
 
-        await client.exportDocuments({
-          name: databaseName,
-          outputUriPrefix: bucket,
-        });
-        console.info('export database, success');
-      } catch (err) {
-        console.error(err);
-      }
-    });
+  await client.exportDocuments({
+    name: databaseName,
+    outputUriPrefix: bucket,
+  });
+  console.info('export database, success');
 };
